5887a8bc6e3d2b61706d34ba452a66ed5be1d2cf
Maxim Velesyuk <mv@createdigital.com>
2014-10-22 19:46:56 +0400

    Fixed get_user_model import.

--- registration/forms.py
+++ registration/forms.py
@@ -9,7 +9,7 @@ you're using a custom model.
 """
 
 
-from django.contrib.auth.models import get_user_model
+from django.contrib.auth import get_user_model
 from django import forms
 from django.utils.translation import ugettext_lazy as _
 
